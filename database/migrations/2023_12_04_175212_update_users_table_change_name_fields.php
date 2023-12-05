47dd09b98449339c6236e5c16ffc3a85f038629d
Osvaldas Beniušis <osvaldas.beniusis@gmail.com>
2023-12-05 07:21:13 +0200

    Remove unused and add missing user fields

---
+++ database/migrations/2023_12_04_175212_update_users_table_change_name_fields.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('email_verified_at');
+            $table->dropRememberToken();
+
+            $table->string('first_name');
+            $table->string('last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+        });
+    }
+};
